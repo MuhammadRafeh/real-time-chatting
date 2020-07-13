f9dfdada9d92fe09dac0c363473fbd88f0da12e4
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2020-07-13 09:10:51 +0300

    Done the main part

--- application.py
+++ application.py
@@ -1,20 +1,27 @@
 import os, time
 import uuid
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-users = []
+users = {} #request.sid, ids
 
 #str(uuid.uuid4().fields[-1])[:5] to take random ids
 
 #Private Channels List
 private_record = {} #using for storing the channel id and the names of users who are currently exist in this channel
 private_info = {}
+
+#to get dict key from values
+def get_key(val):
+	for key, value in users.items():
+		if val == value:
+			return key
+	return 'n'
 
 #Public Channels List
 public_record = {}
@@ -26,14 +33,27 @@ def index():
 
 @socketio.on("connected")
 def connect():
+	print(request.sid)
 	time.sleep(1)
 	while True:
-		id = str(uuid.uuid4().fields[-1])[:5]
-		if id not in users:
-			users.append(id)
-			emit('id generated', {"id": id})
+		ids = str(uuid.uuid4().fields[-1])[:5]
+		if ids not in users:
+			emit('id generated', {"id": ids})
 			break
-	users.append(id)
+	users[request.sid] = ids
+
+@socketio.on("reconnected")
+def reconnect(data):
+	ids = data["cookie"]
+	found = get_key(ids) #getting the key of id that's probabily a socket.id/request.sid
+	if found!='n': #After reconnection we founded the id in server that's good
+		del users[found] #Deleting the previous socket id
+		users[request.sid] = ids #Assigning the new user
+		emit('reconnection success', {'id': ids})
+	else:
+		emit('user voilated')
+	print(users)
+
 
 @socketio.on("private creation")
 def private(data):
@@ -59,7 +79,12 @@ def public(data):
 			emit('public created', {'id': id, 'channel_name': channel})
 			break
 
-	
-	
+@socketio.on("private message")
+def messaging_privately(data):
+	#friend = str(data["friend_id"])
+	#if friend not in users:
+	#	emit('user not found')
+	emit('mssg', room=request.sid)
+	print(request.sid)
 
 
--- static/js/javascript.js
+++ static/js/javascript.js
@@ -14,6 +14,36 @@ if (!localStorage.getItem('id'))
 const private_channel_list = []
 const public_channel_list = []
 
+//Function to chk if user try to change cookie
+var checkCookie = function() {
+
+	try{
+	var lastCookie = document.cookie
+	  .split('; ')
+	  .find(row => row.startsWith('chatshikariid'))
+	  .split('=')[1];
+	  console.log(cookieValue)
+	 }
+	 catch(err){
+	 	console.log('')
+	 }
+    
+
+    return function() {
+
+        var currentCookie = document.cookie;
+
+        if (currentCookie != lastCookie) {
+
+            // something useful like parse cookie, run a callback fn, etc.
+
+            lastCookie = currentCookie; // store latest cookie
+
+        }
+    };
+}();
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
 	//setting the name of user if it's exist
@@ -39,7 +69,6 @@ document.addEventListener('DOMContentLoaded', function() {
 			button.onclick = function(){
 				const channel_name = document.querySelector('#private-input').value;
 				socket.emit('private creation', {'channel_name': channel_name, 'user_id': localStorage.getItem('id')})
-	
 			}
 		}
 		else if (button.dataset.view==='public'){
@@ -48,28 +77,44 @@ document.addEventListener('DOMContentLoaded', function() {
 				socket.emit('public creation', {'channel_name': channel_name, 'user_id': localStorage.getItem('id')})
 			}
 		}
-
-		
 	})
 
-		
+	document.querySelector('#private-message').onclick = () => {
+		const friend_id = prompt("Enter Your Friend's Id")
+		if (friend_id)
+			socket.emit('private message', {'friend_id': friend_id, 'mine_id': localStorage.getItem('id')})
+	}
 	
 
 })
 
-socket.on('connect', () => {
-
-	//if id not assign then execute this if
-	if (localStorage.getItem('id')===''){
-
-		//Event to take unique id from Server
-		socket.emit('connected')
-		//console.log('done')
+socket.on('connect', function(){
+	//socket.id;
+	//socket.username = "rafeh"
+	try{
+	var cookieValue = document.cookie
+	  .split('; ')
+	  .find(row => row.startsWith('chatshikariid'))
+	  .split('=')[1];
+	  console.log(cookieValue)
+	  window.setInterval(checkCookie, 100);
+	}
+	catch(err){
+		console.log('Visited First Time')
 	}
 
-	//assigning the id at bottom left section of webpage
-	document.querySelector('#allocate_id').innerHTML = localStorage.getItem('id')
-	
+	//if id not assign then execute this if
+	if (typeof(cookieValue)==='undefined'){ //New User
+		console.log('undefined')
+		//Event to take unique id from Server
+		socket.emit('connected')
+	}
+
+	else{
+		console.log('called')
+		socket.emit('reconnected', {'cookie': cookieValue})
+	}
+
 })
 
 socket.on('id generated', data => {
@@ -77,9 +122,26 @@ socket.on('id generated', data => {
 	localStorage.setItem('id', data.id);
 	alert(`Your generated id is: ${data.id}`)
 
+	const cname = "chatshikariid";//cookie key 
+	const cvalue = data.id//cookie value
+	const d = new Date();
+	d.setTime(d.getTime() + (5*24*60*60*1000)); //5 day
+
+	const expires = "expires="+ d.toUTCString();
+
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
 	//assigning the id at bottom left section of webpage
 	document.querySelector('#allocate_id').innerHTML = data.id
+	
+	window.setInterval(checkCookie, 100);
 
+})
+
+socket.on('reconnection success', data => {
+		localStorage.setItem('id', data.id)
+		document.querySelector('#allocate_id').innerHTML = localStorage.getItem('id')
+		alert("Welcome Back!")
 })
 
 socket.on('private created', data => {
@@ -94,7 +156,28 @@ socket.on('public created', data => {
 	public_channel_list.push({'id': data.id, 'name': data.channel_name})
 })
 
+socket.on('user not found', () => {
+	alert("User Not Found, Invalid User Id!")
+})
 
+// socket.on('disconnect', () => {
+
+//       socket.emit('disconnected');
+
+//       localStorage.removeItem('id')
+//       localStorage.removeItem('name')
+
+//   })
 // socket.on('id generated', data => {
 // 	console.log(data.id);
 // })
+
+socket.on('mssg', () => {
+	confirm("asd");
+	alert('done')
+})
+
+socket.on('user voilated', () => {
+	alert("You tried to do something Wrong")
+})
+
